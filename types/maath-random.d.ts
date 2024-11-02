949a033e89209f342823bef0c68c5025f5bfd784
Karim248119 <karimoda66@gmail.com>
2024-11-02 14:53:50 +0200

    first commit

---
+++ types/maath-random.d.ts
@@ -0,0 +1,8 @@
+// @types/maath-random.d.ts
+declare module "maath/random/dist/maath-random.esm" {
+  export function inSphere(
+    array: Float32Array,
+    options: { radius: number }
+  ): Float32Array;
+  // Add other exports from "maath/random/dist/maath-random.esm" if needed
+}
